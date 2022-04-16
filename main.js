fa7c3e6a7940f5a54e87c20e03807eb9bc3a355b
Shantanu Gupta <99300527+Shantanugupta43@users.noreply.github.com>
2022-04-16 16:34:48 +0100

    Timetable Module and new degree/Module feat added

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var pageCounter = 1;
 var moduleContainer = document.getElementById('module-info');
 var btn = document.getElementById("btn");
+var addinglist = document.getElementById("addlist")
 
 
 btn.addEventListener("click", function(){
@@ -115,14 +116,20 @@ function moduleHTML(data) {
 
 
         htmlString += "Module led by: " + data[i].Module.Academic[0];
-        htmlString += "<ul><li>" + data[i].Module.Name[0] + "</li>";
-        htmlString += "<li>" + data[i].Module.Name[1] + "</li>";
-        htmlString += "<li>" + data[i].Module.Name[2] + "</li></ul>";
+        htmlString += "<ul><li>" + data[i].Module.Name[0] + "<p> Module Time Slots Each week: </p>" + data[i].Module.Time_table_name1[0] + "</li></ul>";
+        htmlString += "<ul><li>" + data[i].Module.Name[1] + "<p> Module Time Slots Each week: </p>" + data[i].Module.Time_table_name2[0] + "</li></ul>";
+        htmlString += "<ul><li>" + data[i].Module.Name[2] +  "<p> Module Time Slots Each week: </p>" + data[i].Module.Time_table_name3[0] + "</li></ul>";
+        
 
-        htmlString += `<p><form>
-        <label for="fname">Add Module Name:</label><p>
-        <input type="text" id="fname" name="fname">
-        <input type="submit" value="Submit">`;
+
+
+
+
+
+        
+
+
+
 
 
 
@@ -136,7 +143,7 @@ function moduleHTML(data) {
 
     }
   
-  
+
   
   
   
@@ -144,8 +151,36 @@ function moduleHTML(data) {
   }
 
 
+  htmlString += `<p><form>
+  <label for="webdev">Add Module Name in : Web dev  </label><p>
+  <input type="text" id="webdev" name="webdev">`;
 
-    moduleContainer.insertAdjacentHTML('beforeend', htmlString);
+  htmlString += `<p><form>
+  <label for="bigdata">Add Module Name in : Big data  </label><p>
+  <input type="bigdata" id="bigdata" name="bigdata">`;
+
+  htmlString += `<p><form>
+  <label for="bigdata">Add Module Name in : Data Mining  </label><p>
+  <input type="bigdata" id="datamining" name="datamining">`;
+
+
+  b = document.createElement('button');
+  b.innerText = 'Click to update new modules'
+  b.id = 'list'
+  b.addEventListener('click', () => {
+    storenewmodulesHTML();
+      
+  });
+  document.body.appendChild(b)
+
+
+
+  moduleContainer.insertAdjacentHTML('beforeend', htmlString);
+
+
+    
+
+   
 
 
 
@@ -204,6 +239,121 @@ function degreeHTML(data) {
 
 
 
+  e = document.createElement('button');
+  e.innerText = 'Click to Add Degree'
+  e.id = 'degreeadd'
+  e.addEventListener('click', () => {
+    adddegreeHTML();
+  });
+  document.body.appendChild(e)
+
+
+
+
+
+
+
+
+
+
+    moduleContainer.insertAdjacentHTML('beforeend', htmlString);
+
+
+
+
+
+
+}
+
+
+
+function adddegreeHTML() {
+
+  var htmlString = "";
+
+  htmlString += "<h3>" + "Add a Degree and its modules" + "</h3>";
+  htmlString += `<p><form>
+  <label for="degree1">Add a new degree:  </label><p>
+  <input type="text" id="degree1" name="degree1">`;
+
+  htmlString += `<p><form>
+  <label for="module1">Add a New or Existing Module:  </label><p>
+  <input type="text" id="module1" name="module1">`;
+
+
+  f = document.createElement('button');
+  f.innerText = 'Click to Add New Degree and modules'
+  f.id = 'degreeadd'
+  f.addEventListener('click', () => {
+    storedegreeHTML();
+  });
+  document.body.appendChild(f)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    moduleContainer.insertAdjacentHTML('beforeend', htmlString);
+
+
+
+
+
+
+}
+
+function storedegreeHTML() {
+
+  var htmlString = "";
+
+  htmlString += "<p>" + "New degree and module generated!" + "</p>";
+
+
+  degnew = document.getElementById("degree1");
+  modnew = document.getElementById("module1");
+
+
+  htmlString += "<h3>" + degnew.value + "</h3>";
+  htmlString += "<p> Module </p>"
+  htmlString += "<ul><li>" + modnew.value + "</li></ul>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -226,5 +376,63 @@ function degreeHTML(data) {
 
 
 
+function storenewmodulesHTML() {
+
+  var htmlString = "";
+
+  htmlString += "<h3>" + "New modules" + "</h3>";
 
 
+  webdev = document.getElementById("webdev");
+  bigdata = document.getElementById("bigdata");
+  mining = document.getElementById("datamining");
+
+
+
+  htmlString += "<p> New Module added in webdev </p>";
+
+
+  htmlString += "<ul><li>" + webdev.value + "</li></ul>";
+
+  htmlString += "<p> New Module added in Big Data </p>";
+  htmlString += "<ul><li>" + bigdata.value + "</li></ul>";
+
+  htmlString += "<p> New Module added in Data Mining </p>";
+  htmlString += "<ul><li>" + mining.value + "</li></ul>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    moduleContainer.insertAdjacentHTML('beforeend', htmlString);
+
+
+
+
+
+
+}
